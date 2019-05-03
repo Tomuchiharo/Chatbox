368a3c96cd10e5d4029a5f44872b98f844af8be8
Thomas Hoarau <thomas.h1605@gmail.com>
2019-05-03 16:44:12 +0200

    on est trop chaud

--- JS/chatroom.js
+++ JS/chatroom.js
@@ -1,9 +1,33 @@
 $(document).ready(function(){
-	var url = window.location.href; 
-			var myUrl = url.split("=");
-            var myPseudo = myUrl[1];
-            console.log(myPseudo);
-			$('#pseudo').html("<p> Connecté en tant que"+ myPseudo+"</p>")
 
+	infoSession();
+	$('#btnMsg').on('click', chatMsg);
+	
 });
 
+ var url = window.location.href; 
+			var myUrl = url.split("=");
+            var myPseudo = myUrl[1];
+            var myChatroom = myUrl[3];
+
+	function infoSession(){
+          
+			$('#pseudo').html("<p> Connecté en tant que <strong>"+ myPseudo+"</strong></p>")
+			$('#groupe').html(" Discussion : "+ myChatroom );
+	}
+
+
+	function chatMsg(){
+		   var msg = $("#inputMsg").val();
+
+		   $.ajax({
+		       url : 'chatMsg.php',
+		       method : 'post',
+		       dataType: 'json',
+		       data : {msg : msg, pseudo : myPseudo, chat : myChatroom},
+		       success:function(data){
+		             console.log(data);
+
+		       }
+		   });
+		}
--- chatMsg.php
+++ chatMsg.php
@@ -1,15 +1,38 @@
 <?php
 include "connexion_bdd.php";
 
-$name = $_POST['msg'];
+$msg = $_POST['msg'];
+$chat = $_POST['chat'];
+$pseudo = $_POST['pseudo'];
+
+$requete = $pdo->prepare("SELECT `Id` FROM `Chat` WHERE `chatName`=?");
+
+$requete->execute([$chat]);
+
+$chatName = $requete->fetch(PDO::FETCH_ASSOC);
+
+$requete = $pdo->prepare("SELECT `Id` FROM `User` WHERE `Pseudo`=?");
+
+$requete->execute([$pseudo]);
+
+$myPseudo = $requete->fetch(PDO::FETCH_ASSOC);
+
+/*
+("SELECT `User_Id`, `chat_Id` FROM `msg` INNER JOIN Chat ON msg.chat_Id = Chat.Id INNER JOIN User ON msg.User_Id = User.Id WHERE chatName = ? AND Pseudo = ?
+");
+
+$requete->execute([$chat, $pseudo]);
+
+$requete = $requete->fetch(PDO::FETCH_ASSOC);
+
+echo $requete;*/
 
 $requete = $pdo->prepare("
 INSERT INTO `msg`(`User_Id`, `Content`, `chat_Id`) VALUES (?, ?, ?)
 ");
 
-$requete->execute([$name]);
+$data = $requete->execute([$myPseudo['Id'], $msg, $chatName['Id']]);
 
-echo json_encode($name);
-
+echo json_encode($data);
 
 ?>
