ceeda7438051889f5a7b0fd977b77f0896dfcd17
lea <lea.odienne@gmail.com>
2019-05-03 15:24:19 +0200

    voila2

---
+++ createChat.php
@@ -0,0 +1,15 @@
+<?php
+include "connexion_bdd.php";
+
+$name = $_POST['chatroom'];
+
+$requete = $pdo->prepare("
+    INSERT INTO `Chat`(`chatName`) VALUES ?
+");
+
+$requete->execute([$name]);
+
+echo json_encode($name);
+
+
+?>
