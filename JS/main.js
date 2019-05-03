8d82d6e8236a393fd3e90beafc437761bc154e09
Thomas Hoarau <thomas.h1605@gmail.com>
2019-05-03 15:38:23 +0200

    pif paf pouf

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,9 @@
 $(document).ready(function(){
-	$('#linklogin').on('click', changeBox);
-	$('#linkBox').on('click','div a', changeBox1);
 	$('#btnJoin').on('click', onclickjoin);
-	$('#btnBox').on('click','div button', onclickPseudo);
+	$('#btnPseudo').on('click', onclickPseudo);
+	$('#btnCreate').on('click', onclickCreate);
 
 });
-
-
-function changeBox(){
-
-	$('#labelbox').html('<h3 class="text-info" id="create"><i class="fas fa-lock"></i> Créer une discussion :</h3>');
-
-	$('#btnBox').html('<button type="button" class="btn btn-info col-md-12" > Créer</button>');
-
-	$('#linkBox').html('<a href="#" class="text-info" id="linklogin1">Rejoindre une discussion</a>')
-}
-
-function changeBox1(){
-
-	$('#labelbox').html('<h3 class="text-info" id="create"><i class="fas fa-lock"></i> Rejoindre une discussion :</h3>');
-
-	$('#btnBox').html('<button type="button" class="btn btn-info col-md-12" > Rejoindre</button>');
-
-	$('#linkBox').html('<a href="#" class="text-info" id="linklogin">Créer une nouvelle une discussion</a>')
-}
-
 
 
 function onclickjoin(){
@@ -41,12 +20,7 @@ function onclickjoin(){
 			  $('#badName').fadeIn();
 		  }else {
 			$('#badName').fadeOut();
-			$('#labelbox').html('<h3 class="text-info" id="create"><i class="fas fa-user"></i> Votre pseudo </h3>');
-
-			$('#btnBox').html('<div><button type="button" id="btnPseudo" class="btn btn-info col-md-12" > Rejoindre</button></div>');
-
-			$('#inputBox').html('<input type="text" class="form-control" id="inputlogin">')
-			
+			$(location).attr('href',"pseudo.html?chatroom="+chatroom);
 
 		  }
 		}
@@ -58,7 +32,7 @@ function onclickjoin(){
 function onclickPseudo(){
 	var url = window.location.href; 
 			var myUrl = url.split("=");
-            var myChatRoom = myUrl[1];
+            var chatroom = myUrl[1];
             
 	var pseudo = $('#inputlogin').val();
 
@@ -68,9 +42,26 @@ function onclickPseudo(){
 		dataType: 'json',
 		data : {pseudo : pseudo},
 		success:function(data){
-			$(location).attr('href',"chat.html?name="+pseudo);
+			$(location).attr('href',"chat.html?name="+pseudo+"=chatroom="+chatroom);
 			
 		}
 	});
+}
+
+function onclickCreate(){
+
+    var chatroom = $('#inputlogin').val();
+
+    $.ajax({
+        url : 'createChat.php',
+        method : 'post',
+        dataType: 'json',
+        data : {chatroom : chatroom},
+        success:function(data){
+              console.log('ok');
+        }
+    });
+
+
 }
 
