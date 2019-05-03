1f78e4c693f6228b34f9d16a8e90994ce2734e52
lea <lea.odienne@gmail.com>
2019-05-03 11:48:18 +0200

    bon nom

---
+++ join.php
@@ -0,0 +1,19 @@
+<?php
+include "connexion_bdd.php";
+
+$name = $_POST;
+
+$requete = $pdo->prepare("
+    SELECT `Id` FROM `Chat` WHERE `chatName`= ? 
+");
+
+$requete->execute([$name]);
+
+
+$infos = $requete->fetch(PDO::FETCH_ASSOC);
+
+
+echo json_encode($infos);
+
+
+?>
